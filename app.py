3479510b5d69f961711985b3d7748c3640e2276b
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2023-09-27 08:01:57 -0500

    consulta dos tablas

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify,json
 from config.db import  db, ma, app
-from api.cliente import ruta_clientes
-from api.reserva import ruta_reservas
+from api.cliente import Cliente, ruta_clientes
+from api.reserva import Reserva, ruta_reservas
 
 app.register_blueprint(ruta_clientes,url_prefix = '/api')
 app.register_blueprint(ruta_reservas, url_prefix = '/api')
@@ -9,6 +9,20 @@ app.register_blueprint(ruta_reservas, url_prefix = '/api')
 @app.route('/')
 def index():
     return "Hola Mundo"
+
+@app.route('/dostablas', methods=['GET'])
+def dostabla():
+    datos = {}
+    resultado = db.session.query(Cliente, Reserva). \
+        select_from(Cliente).join(Reserva).all()
+    i=0
+    for clientes, reservas in resultado:
+        i+=1
+        datos[i]={
+            'cliente':clientes.nombre,
+            'reserva': reservas.id
+        }
+    return datos
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
