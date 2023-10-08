561a9882bb8a209920849d7b50ff2040b3de0925
Notonyus_risk <antoniojoserudassanchez@gmail.com>
2023-10-08 17:34:11 -0500

    update

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, jsonify,json
 from config.db import  db, ma, app
 from api.cliente import Cliente, ruta_clientes
 from api.reserva import Reserva, ruta_reservas
-#from api.aerolinea import Aerolinea, ruta_aerolinea
-#from api.aeropuerto import Aeropuerto, ruta_aeropuertos
-#from api.avion import Avion, ruta_avion
-#from api.ciudad import Ciudad, ruta_ciudades
 
 app.register_blueprint(ruta_clientes,url_prefix = '/api')
 app.register_blueprint(ruta_reservas, url_prefix = '/api')
